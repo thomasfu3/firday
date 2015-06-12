13153a0d5e72968ffd4fb3c2edbc475071b82f64
thomas fu <tfu@ebates.com>
2015-06-12 13:40:25 +0800

    est

--- a.php
+++ a.php
@@ -1,3 +1,19 @@
 <?php
-phpinfo();
-?>
+
+require 'vendor/autoload.php';
+
+use ExtrabuxApi\Client;
+
+$key = "a3d894c1f99b3508724817b80c2ffc6e";
+$secret = "7b8f8ae5dc589f395c97eba4a7a77e9fbd3ebd3d";
+$baseUrl = 'http://www.extrabux.com';
+$client = Client::create($key, $secret, $baseUrl);
+
+$response = $client->getMerchantList(['limit' => 4, 'page' => 2]);
+$data = $response->toArray();
+print_r($data);
+exit;
+
+$merchantResponse = $client->getMerchant(['id' => 3]);
+$merchantData = $merchantResponse->toArray();
+print_r($merchantData);
--- b.php
+++ b.php
@@ -1,3 +1,33 @@
 <?php
-aaa;
-?>
+//$content = 'You already submitted a transaction inquiry for this Order ID. Please check your Transaction Inquiry log for status.';
+//$content = 'all.orderinquiry.error.orderid.already.exists';
+//preg_match('/orderid\.already\.exists|You\s*already\s*submitted\s*a\s*transaction/i', $content, $matches);
+//print_r($matches);
+//exit;
+
+class test {
+    protected $note;
+    
+    public function note($note)
+    {
+        $this->note = $note;
+    }
+}
+
+$temp = new test();
+$temp->note('aaa');
+$tests[] = $temp;
+
+$temp = new test();
+$temp->note('bbb');
+$tests[] = $temp;
+
+$temp = new test();
+$temp->note('ccc');
+$tests[] = $temp;
+
+print_r($tests);
+$temp = $tests[1];
+$temp->note('wo shi test');
+print_r($tests);
+
